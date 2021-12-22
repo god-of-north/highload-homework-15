4aa0a75d3315de91d2fb90d78cce768ecc71a5c0
Maksym Lesyk <mnl@industrialinnovationgroup.com>
2021-12-22 21:36:47 +0200

    config for elastic and kibana

--- services/logstash/pipeline/run.bat
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --name logstash -v "D:\Temp\Projector\hw15\highload-homework-15\services\logstash\pipeline":/app --entrypoint bash logstash:7.16.1
--- services/logstash/pipeline/std.bat
+++
@@ -1,1 +0,0 @@
-docker run -it --rm logstash:7.16.1 logstash -e "input { stdin { } } output { stdout { } }"
